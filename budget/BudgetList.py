18831f05ebba9fe55ff29cb4ffa001f80cef644e
Starley Flynn <starley.flynn@salesforce.com>
2021-10-21 16:45:12 -0700

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -9,7 +9,7 @@ class BudgetList():
         self.sum_overages = 0
         self.overages = []
 
-    def __append__(self, item):
+    def append(self, item):
         if((self.sum_expenses + item) < self.budget):
             self.expenses.append(item)
             self.sum_expenses += item
